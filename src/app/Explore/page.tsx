386314ef04bc83c4418a80b5b44056ab14fad969
Moses Handoyo <moses.handoyo99@gmail.com>
2025-10-23 20:29:59 +0700

    fix: fix the houdini page

--- src/app/Explore/page.tsx
+++ src/app/Explore/page.tsx
@@ -3,25 +3,26 @@
 import { useEffect } from "react";
 import { HeroSection } from "./hero"
 export default function Portfolio() {
-  return (
-      useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-          if (event.key === "a") {
-            window.location.href = "/Experience";
-          } else if (event.key === "s") {
-            window.location.href = "/Education";
-          }  else if (event.key === "w") {
-            window.location.href = "/Explore";
-          }
-        }
-        
-        window.addEventListener('keydown', handleKeyDown);
-        return () => {
-        window.removeEventListener('keydown', handleKeyDown);
-      };
-      }, []),
-    <div className="min-h-screen bg-background">
-      <HeroSection />
-    </div>
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "a") {
+        window.location.href = "/Experience";
+      } else if (event.key === "s") {
+        window.location.href = "/Education";
+      }  else if (event.key === "w") {
+        window.location.href = "/Explore";
+      }
+    }
+    
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+    window.removeEventListener('keydown', handleKeyDown);
+  };
+  }, []);
+  
+  return(
+  <div className="min-h-screen bg-background">
+    <HeroSection />
+  </div>
   )
 }
